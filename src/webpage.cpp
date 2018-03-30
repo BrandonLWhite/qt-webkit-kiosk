859619e72de483459c4bba0d2aec805a171831dd
Brandon White <brandonlwhite@gmail.com>
2018-03-30 15:04:54 -0500

    TODO Comments

--- src/webpage.cpp
+++ src/webpage.cpp
@@ -8,5 +8,8 @@ WebPage::WebPage(QObject* parent): QWebPage(parent)
 
 void WebPage::javaScriptConsoleMessage(const QString& message, int lineNumber, const QString& sourceID)
 {
+    // TODO BW: Need a better format.
+    // TODO BW: Need to make this optional via a condition.  But don't check every time.
+    //          Instead, use a bool flag and a setter.
     QTextStream(stdout) << message << lineNumber << sourceID << endl;
 }
